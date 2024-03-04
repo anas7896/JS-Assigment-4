5c66855fe45b4f91e9d92d9fa6e04f32c73c7407
anas7896 <anasawan0092@gmail.com>
2024-03-04 17:47:26 +0500

    JS-Assigment-4 done

---
+++ app.js
@@ -0,0 +1,54 @@
+// Chapter No 1 - Array
+
+// Q NO: 1
+
+// var stringsArray = ["apple", "banana", "orange", "watermellon"];
+// console.log(stringsArray)
+
+// Q NO: 2
+
+// var numArray = ["1", "2", "3", "4"];
+// console.log(numArray)
+
+// Q NO: 3
+
+// var booleanArray = ["True", "False", "False", "True"];
+// console.log(booleanArray)
+
+// Q NO: 4
+
+// var mixArray = ["apple", "1", "True", "Cat"];
+// console.log(mixArray)
+
+// Q NO: 5
+
+// var Qualification = ['SSC', 'HSC', 'BCS', 'BS', 'BCOM', 'MS', 'M.Phil.', 'PHD'];
+
+// var nestedArray = '<h1>Qualification</h1><br><ol>' +
+//                 '<li>' + Qualification[0] + '</li>' +
+//                 '<li>' + Qualification[1] + '</li>' +
+//                 '<li>' + Qualification[2] + '</li>' +
+//                 '<li>' + Qualification[3] + '</li>' +
+//                 '<li>' + Qualification[4] + '</li>' +
+//                 '<li>' + Qualification[5] + '</li>' +
+//                 '<li>' + Qualification[6] + '</li>' +
+//                 '<li>' + Qualification[7] + '</li>' +
+//               '</ol>';
+
+// document.write(nestedArray);
+
+// Q NO: 6
+
+// var student = ['Micheal', 'John', 'Tony'], marks = [320, 230, 480]
+// var totalMarks = 500
+
+// document.write('Score of ' + student[0] + ' is  ' + marks[0] + '. Percentage: ' + (marks[0]/totalMarks)* 100 + '%' + '<br>')
+
+// document.write('Score of ' + student[1] + ' is  ' + marks[1] + '. Percentage: ' + (marks[1]/totalMarks)* 100 + '%' + '<br>')
+
+// document.write('Score of ' + student[2] + ' is  ' + marks[2] + '. Percentage: ' + (marks[2]/totalMarks)* 100 + '%')
+
+// Q NO: 7
+
+var color = ['red', 'green', 'blue']
+
