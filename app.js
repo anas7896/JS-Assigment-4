cfa135f609d73dd00b8efee3da4fb10789307140
anas7896 <anasawan0092@gmail.com>
2024-03-20 23:05:09 +0500

    assigment done

--- app.js
+++ app.js
@@ -42,13 +42,104 @@
 // var student = ['Micheal', 'John', 'Tony'], marks = [320, 230, 480]
 // var totalMarks = 500
 
-// document.write('Score of ' + student[0] + ' is  ' + marks[0] + '. Percentage: ' + (marks[0]/totalMarks)* 100 + '%' + '<br>')
+// document.write('Score of ' + student + ' is  ' + marks[0] + '. Percentage: ' + (marks[0]/totalMarks)* 100 + '%' + '<br>')
 
-// document.write('Score of ' + student[1] + ' is  ' + marks[1] + '. Percentage: ' + (marks[1]/totalMarks)* 100 + '%' + '<br>')
+// document.write('Score of ' + student + ' is  ' + marks[1] + '. Percentage: ' + (marks[1]/totalMarks)* 100 + '%' + '<br>')
 
-// document.write('Score of ' + student[2] + ' is  ' + marks[2] + '. Percentage: ' + (marks[2]/totalMarks)* 100 + '%')
+// document.write('Score of ' + student + ' is  ' + marks[2] + '. Percentage: ' + (marks[2]/totalMarks)* 100 + '%')
 
 // Q NO: 7
 
-var color = ['red', 'green', 'blue']
+// var score = [320, 230, 480, 120]
+
+// score.sort()
+
+// document.write('score of students: ' + score)
+
+// Q NO: 8
+
+// var citiess = ['Karachi', 'Islamabad', 'Lahore', 'Quetta', 'Peshawar']
+// var cities = ['Karachi', 'Islamabad', 'Lahore', 'Quetta', 'Peshawar']
+
+// selectedCities = cities.splice(3)
+
+// document.write('Cities list:' + '<br>' + citiess + '<br>')
+// document.write('Selected Cities list:' + '<br>' + cities)
+
+// Q NO: 9
+
+// var arr = ['This', 'is', 'my', 'cat']
+// var singleString = arr.join(" ");
+// document.write('Array:' + '<br>' + arr + '<br>')
+// document.write('String:' + '<br>' + singleString)
+
+// Q NO: 10
+
+// Chapter 2 Array and loop
+
+// Q NO: 1
+
+// for (var i = 1; i <= 10; i++){
+//     document.write(i + '<br>')
+// }
+
+// Q NO: 2
+
+// var tableNumber = +prompt("Enter table number")
+// var tableLength = +prompt("Enter table length")
+
+// for (var i = 1; i <= tableLength; i++) {
+//     document.write(tableNumber + " * " + i + " = " + tableNumber * i + '<br> ');
+// }
+
+// Q NO: 3
+
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"]
+
+// for (var i = 0; i < fruits.length; i++) {
+//     document.write(fruits[i] + '<br>')
+// }
+
+// document.write("<br>")
+// document.write("Element at index " + fruits.indexOf("apple") + ' is ' + fruits[0] + '<br>')
+// document.write("Element at index " + fruits.indexOf("banana") + ' is ' + fruits[1] + '<br>')
+// document.write("Element at index " + fruits.indexOf("mango") + ' is ' + fruits[2] + '<br>')
+// document.write("Element at index " + fruits.indexOf("orange") + ' is ' + fruits[3] + '<br>')
+// document.write("Element at index " + fruits.indexOf("strawberry") + ' is ' + fruits[4] + '<br>')
+
+// Q NO: 4
+
+// document.write("<strong>Counting:</strong>" + "<br>");
+// for (var i = 1; i <= 15; i++) {
+//     document.write(i + ", ");
+// }
+// document.write("<br>");
+
+
+// document.write("<strong> Reverse counting:</strong>" + "<br>");
+// for (var i = 10; i >= 1; i--) {
+//     document.write(i + ", ");
+// }
+// document.write("<br>");
+
+
+// document.write("<strong>Even:</strong>" + "<br>");
+// for (var i = 0; i <= 20; i += 2) {
+//     document.write(i + ", ");
+// }
+// document.write("<br>");
+
+
+// document.write("<strong>Odd:</strong>" + "<br>");
+// for (var i = 1; i <= 19; i += 2) {
+//     document.write(i + ", ");
+// }
+// document.write("<br>");
+
+
+// document.write("<strong>Series:</strong>" + "<br>");
+// for (var i = 2; i <= 20; i += 2) {
+//     document.write(i + "k, ");
+// }
+
 
